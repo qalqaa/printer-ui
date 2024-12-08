1c4011c5423ceb73cd88ba676c9b01f0cc1eaa7c
qalqa <andreybas04@gmail.com>
2024-12-08 21:36:28 +0300

    fix: small fixes

--- src/components/Printer/PrinterCard/PrinterCard.vue
+++ src/components/Printer/PrinterCard/PrinterCard.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
+import CoilCard from '@/components/Coil/card/CoilCard.vue'
 import DialogWindow from '@/components/DialogWindow/DialogWindow.vue'
 import { fetchRequest } from '@/data/api/api'
 import type { ICoil, IPrinter } from '@/model/interfaces'
-import { ref } from 'vue'
+import { coilsKey } from '@/util/injectionKeys'
+import { inject, ref } from 'vue'
 
 const props = defineProps<IPrinter>()
 
 const isPrinting = ref(false)
 
+const coils = inject(coilsKey)
+
+if (!coils) {
+  throw new Error('Printers service is not provided')
+}
+
+const { coilsData } = coils
+
 const isRefillMode = ref(false)
-const coilData = ref<ICoil[]>([])
-const selectedCoil = ref<ICoil>()
+const selectedCoil = ref<ICoil | 'placeholder'>('placeholder')
 
 const refillHandle = () => {
-  fetchRequest<ICoil[]>({ url: '/coils', method: 'GET' }).then(
-    (response) => (coilData.value = response),
-  )
-  if (coilData.value.length === 0) {
+  if (coilsData.value.length === 0) {
     console.log('no coils')
     return
   }
@@ -24,16 +30,19 @@ const refillHandle = () => {
 }
 
 const refill = () => {
-  if (selectedCoil.value === undefined) return
-  fetchRequest<ICoil>({
-    url: `/printers/${props.id}`,
-    method: 'PUT',
-    data: {
-      ...props,
-      coil: selectedCoil.value,
-    },
-  })
-  fetchRequest<ICoil>({ url: `/coils/${selectedCoil.value.id}`, method: 'DELETE' })
+  if (selectedCoil.value !== 'placeholder') {
+    fetchRequest<ICoil>({
+      url: `/printers/${props.id}`,
+      method: 'PUT',
+      data: {
+        ...props,
+        coil: selectedCoil.value,
+      },
+    })
+    fetchRequest<ICoil>({ url: `/coils/${selectedCoil.value.id}`, method: 'DELETE' })
+    return
+  }
+  console.error('choose a coil')
 }
 </script>
 
@@ -47,7 +56,7 @@ const refill = () => {
     </div>
     <div v-else class="bg-red flex flex-column px-2 py-3 border-round-lg">
       <h3 class="c-accent">Coil</h3>
-      <coil
+      <CoilCard
         :printer-props="props"
         :id="coil.id"
         :material="coil.material"
@@ -62,9 +71,10 @@ const refill = () => {
   </li>
   <DialogWindow :isOpen="isRefillMode" @close="refillHandle" :onConfirmAction="refill">
     <template #content>
-      <select v-model="selectedCoil" name="coil" id="">
+      <select v-model="selectedCoil" name="coil">
+        <option selected disabled hidden value="placeholder">Chose a coil</option>
         <option
-          v-for="item in coilData"
+          v-for="item in coilsData"
           :key="item.id"
           :value="item"
           :label="item.material + ', ' + item.color + ', ' + item.length + 'm'"
