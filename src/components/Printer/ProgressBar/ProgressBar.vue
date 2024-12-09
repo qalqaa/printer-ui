cfa0d230a531896d3f4c601835cc2bf8bcd4a1dc
qalqa <andreybas04@gmail.com>
2024-12-09 19:23:31 +0300

    feat: added dummy progress bar comp

---
+++ src/components/Printer/ProgressBar/ProgressBar.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  progress: number
+}>()
+
+// const computedWidth = computed(() => `${props.progress}%`)
+</script>
+
+<template>
+  <div class="progress-bar w-full">
+    <div class="progress-bar__fill h-full" :style="{ width: `${props.progress}%` }"></div>
+  </div>
+</template>
+
+<style scoped>
+.progress-bar {
+  height: 5px;
+  border-radius: 10px;
+  overflow: hidden;
+  position: relative;
+}
+
+.progress-bar__fill {
+  background-color: var(--accent);
+  width: 0;
+  transition: width 0.3s ease-in-out;
+}
+</style>
