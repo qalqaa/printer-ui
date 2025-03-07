638417e5b178f8b0191eef818cc8bc823a483206
qalqa <andreybas04@gmail.com>
2025-03-07 21:39:18 +0300

    fix: fixed all trashy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/printer-ui/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
